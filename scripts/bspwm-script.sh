657614de227d1482ccf40ba419e9ea4e25c52130
pokerhawk <eliabedosreis@gmail.com>
2022-08-21 17:52:13 -0300

    cleaning script/not done

--- scripts/bspwm-script.sh
+++ scripts/bspwm-script.sh
@@ -8,18 +8,9 @@ yes|sudo pacman -S xorg xorg-xinit xorg-xsetroot bspwm sxhkd
 clear
 
 # INSTALANDO PACKAGES
-
-echo "INSTALLING TERMINATOR"
-sudo pacman -S terminator
-clear
-echo "INSTALLING ALACRITTY"
-sudo pacman -S alacritty
-clear
+##### AUTO YES PACKAGES
 echo "INSTALANDO PACK DOS ESSENCIAIS"
-yes|sudo pacman -S git nano rofi bpytop htop ranger polybar feh neofetch leafpad picom ttf-liberation ffmpegthumbnailer maim xclip slock nomacs ttf-nerd-fonts-symbols ctags
-clear
-echo "INSTALLING SYNTAX HIGHLIGHTER(NOT NEEDED)"
-sudo pacman -S nano-syntax-highlighting
+yes|sudo pacman -S nano rofi bpytop htop ranger polybar feh neofetch leafpad picom ffmpegthumbnailer maim xclip slock nomacs ttf-nerd-fonts-symbols ctags
 clear
 echo "INSTALLING PULSEAUDIO AND PAVUCONTROL"
 yes|sudo pacman -S pulseaudio pavucontrol
@@ -27,17 +18,21 @@ clear
 echo "INSTALLING LXAPPEARANCE"
 yes|sudo pacman -S lxappearance
 clear
+echo "INSTALLING BASE-DEVELS"
+yes|sudo pacman -S --needed base-devel
+clear
+#libs
+echo "INSTALANDO LIBS/FONTS AND EXTRA SHIT"
+yes|sudo pacman -S sndio portaudio wavpack lib32-libpulse ttf-nerd-fonts-symbols noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk lib32-libpulse lib32-alsa-plugins
+clear
+
+#NON-AUTO PACKAGES
+
+echo "INSTALLING SYNTAX HIGHLIGHTER(NOT NEEDED)"
+sudo pacman -S nano-syntax-highlighting
+clear
 echo "INSTALLING RADEONTOP"
 sudo pacman -S radeontop
-clear
-echo "INSTALLING NAUTILUS"
-sudo pacman -S nautilus
-clear
-echo "INSTALLING PCMANFM AND GVFS"
-sudo pacman -S pcmanfm gvfs
-clear
-echo "INSTALLING THUNAR"
-sudo pacman -S thunar gvfs
 clear
 echo "INSTALLING NUMLOCKX"
 sudo pacman -S numlockx
@@ -58,16 +53,51 @@ clear
 echo "INSTALLING NVIM >> VSCODE"
 sudo pacman -S neovim
 clear
+
+#IF CONDITION PACKAGES
+
+echo "INSTALLING PCMANFM AND GVFS"
+sudo pacman -S pcmanfm gvfs
+clear
+if [ -f "/usr/bin/pcmanfm" ]; then
+	echo "pcmanfm installed"
+else
+	echo "INSTALLING THUNAR"
+	sudo pacman -S thunar gvfs
+	clear
+	echo "INSTALLING NAUTILUS"
+	sudo pacman -S nautilus
+	clear
+fi
+echo "INSTALLING ALACRITTY"
+sudo pacman -S alacritty
+clear	  #mudar isso no futuro para algo diferente
+if [ -f "/usr/bin/alacritty" ]; then
+	echo "alacritty installed"
+else
+	echo"INSTALLING TERMINATOR"
+	sudo pacman -S terminator
+	clear
+fi
+
 echo "INSTALLING CHROMIUM"
 sudo pacman -S chromium
 clear
-echo "INSTALLING FIREFOX"
-sudo pacman -S firefox
-clear
-echo "INSTALLING BASE-DEVELS"
-yes|sudo pacman -S --needed base-devel
+if [ -f "/usr/bin/chromium" ]; then
+	echo "chromium installed"
+else
+	echo "INSTALLING FIREFOX"
+	sudo pacman -S firefox
+	clear
+fi
+
+#INSTALLING LUTRIS ESSENTIALS
+
+echo "INSTALANDO PACOTES ESSENCIAIS PARA LUTRIS/AMD DRIVES AND ELSE"
+sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
 clear
 
+##CONFIG FILES
 # MOVENDO XINIT
 
 cp -v ../xinit-bashrc/bspwm/.xinitrc ~/.xinitrc
@@ -114,6 +144,7 @@ if [[ -f "/home/pk/.moc/config" ]]; then
 	#cp /usr/share/doc/moc/config.example ~/.moc/config
 	#echo "XTermTheme = transparent-background" >> ~/.moc/config
 fi
+
 echo "COPIANDO ADWAITA-DARK THEM AND SWEET-CURSORS TO /usr/share/"
 sudo cp -r -v ../Adwaita-dark /usr/share/themes/
 sudo cp -r -v ../Sweet-cursors /usr/share/icons/
@@ -136,8 +167,7 @@ fi
 
 if [ -f "/usr/bin/terminator" ]; then
 	cp -r -v ../config/terminator ~/.config/
-fi
-if [ -f "/usr/bin/alacritty" ]; then
+elif [ -f "/usr/bin/alacritty" ]; then ## testing//if stop working => change back to 2 if 
 	cp -r -v ../config/alacritty ~/.config/
 fi
 
@@ -147,23 +177,10 @@ cp -r -v ../config/nvim/ ~/.config/
 cp -r -v ../config/extra_config_files/ ~/.config/
 chmod +x ~/.config/extra_config_files/bspwm_smart_move
 
-#INSTALLING LIBS
-
-echo "INSTALANDO LIBS/FONTS AND EXTRA SHIT"
-yes|sudo pacman -S sndio portaudio wavpack lib32-libpulse ttf-nerd-fonts-symbols noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk
-clear
-
-#INSTALLING LUTRIS ESSENTIALS
-
-echo "INSTALANDO PACOTES ESSENCIAIS PARA LUTRIS/AMD DRIVES AND ELSE"
-sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-libpulse lib32-alsa-plugins
-clear
-
 #FIM
 
-echo && echo && echo && echo && echo && echo
+clear
 echo "!!See the README file for extra packages!!"
-echo && echo && echo && echo && echo && echo
 echo "Reboot the system? [Y/n]"
 read resposta
 if [ $resposta == "y" ]||[ $resposta == "Y" ]; then
