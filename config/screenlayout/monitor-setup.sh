298321a29270c071e0b014b3d2e088979833c4d4
pokerhawk <eliabedosreis@gmail.com>
2025-01-09 22:05:19 -0300

    new PC conf

---
+++ config/screenlayout/monitor-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --mode 2560x1440 --rate 165 --pos 1080x373 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right
