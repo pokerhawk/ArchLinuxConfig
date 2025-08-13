dd4ccc3f12411f344558b18572f58a381e4927d1
pokerhawk <eliabedosreis@gmail.com>
2025-08-13 07:15:51 -0300

    secure-boot sh file

---
+++ enable-secure-boot.sh
@@ -0,0 +1,48 @@
+#!bin/sh
+
+# install secure boot ctl:
+sudo pacman -S sbctl
+
+## check status:
+sudo sbctl status
+## should display: ##
+
+# installed: sbctl is not installed
+# setub mode: enabled
+# secure boot: disabled
+
+# create the keys
+sudo sbctl create-keys
+
+# add microsoft-windows signatures
+sudo sbctl enroll-keys -m
+
+# signing key to bootloader (systemd)
+# sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+
+# signing key to bootloader (GRUB)
+sudo sbctl sign -s -o /boot/efi/EFI/Archlinux/grubx64.efi.signed /boot/efi/EFI/Archlinux/grubx64.efi
+
+# replace old unsigned with new signed
+sudo mv /boot/efi/EFI/Archlinux/grubx64.efi /boot/efi/EFI/Archlinux/grubx64.efi.unsigned-backup
+sudo mv /boot/efi/EFI/Archlinux/grubx64.efi.signed /boot/efi/EFI/Archlinux/grubx64.efi
+
+# to know where is the linux kernel image:
+cat /etc/mkinitcpio.d/linux.preset
+# ALL_kver for grub (older setups)
+# default_uki for systemd
+
+# signing the key to kernel (systemd)
+# sudo sbctl sign -s /efi/EFI/Linux/arch-linux.efi
+
+# signing the key to kernel (grub)
+sudo sbctl sign -s /boot/vmlinuz-linux
+
+# install bootloader (systemd)
+# sudo bootctl install
+
+# install bootloader (grub)
+# maybe
+#sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Archlinux
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
+#sudo grub-install
